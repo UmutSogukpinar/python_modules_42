9dddde28f32b4892c61fe17505438829ee77b94c
Umut Sogukpinar <usogukpinar0@gmail.com>
2026-01-05 17:29:17 +0300

    fix: handle bugs on module_00

--- p_00/ex5/ft_count_harvest_iterative.py
+++ p_00/ex5/ft_count_harvest_iterative.py
@@ -1,6 +1,6 @@
 def ft_count_harvest_iterative() -> None:
     day : int = int(input("Days until harvest: "))
-    for i in range(start=1, stop=day + 1):
+    for i in range(1, day + 1):
         print(f"Day {i}")
 
     print("Harvest time!")
--- p_00/ex7/ft_seed_inventory.py
+++ p_00/ex7/ft_seed_inventory.py
@@ -1,11 +1,11 @@
 def ft_seed_inventory(seed : str, amount : int, quantity: int) -> None:
     match quantity:
         case "packets":
-            print(f"{seed} seeds: {amount} packets available")
+            print(f"{seed.capitalize()} seeds: {amount} packets available")
         case "grams":
-            print(f"{seed} seeds: {amount} grams total")
+            print(f"{seed.capitalize()} seeds: {amount} grams total")
         case "area":
-            print(f"{seed} seeds: covers {amount} square meter")
+            print(f"{seed.capitalize()} seeds: covers {amount} square meter")
         case _:
             print("Unknown quantity!")
 
